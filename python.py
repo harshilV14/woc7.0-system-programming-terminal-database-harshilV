844f4930d7264450ffd0eea3d2f6ae41a2f3b42f
harshilV14 <165616960+harshilV14@users.noreply.github.com>
2025-01-22 05:10:25 +0000

    Added functions

--- python.py
+++ python.py
@@ -1,5 +1,25 @@
 database = {}
 
+# Funtctions 
+def setKeyValue(key, val):      # Set Key - Value pair
+    database[key] = val
+    print(f"Key '{key}' set."
+
+def getValue(key):              # Get value ny passing key
+    val = database.get(key, None)
+    if val is not None:
+        print(f"{key}: {val}")
+    else:
+        print(f"Key '{key}' not found.")
+
+def deleteKey(key):            # delete key with value from db
+    if key in database:
+        del database[key]
+        print(f"Key '{key}' deleted.")
+    else:
+        print(f"Key '{key}' not found.")
+
+
 def main():
     while True:
         command = input("DB>").strip()
